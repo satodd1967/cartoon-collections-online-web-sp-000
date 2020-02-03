134ae07b61b52b00e46b8a12f6c0f26f0ac9dc77
Seth Todd <seth.todd@outlook.com>
2020-02-03 03:40:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? d0 |calls|
+  
 end
 
 def find_the_cheese# code an argument here
