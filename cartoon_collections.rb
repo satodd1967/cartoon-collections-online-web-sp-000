3018a8b8fbb91793535bf08e356c3b077d9c0506
Seth Todd <seth.todd@outlook.com>
2020-02-03 04:06:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.index do |cheese|
-    cheese == cheese_types[0] || cheese = cheese_types[1] || cheese == cheese_types [2]
+    cheese == "gouda"
   end
 end
 
